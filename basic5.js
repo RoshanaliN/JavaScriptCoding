131e4ee0d818847df63defef3aea08dc880c811e
RoshanaliN <ec-roshanali.narsindani@ninjavan.co>
2025-09-06 22:37:27 +0530

    string and its methods

---
+++ basic5.js
@@ -0,0 +1,36 @@
+let day = 'tuesday '
+console.log(day.length)
+console.log(day.slice(0, 3))
+console.log(day[2])
+console.log(day.split('s'))
+console.log(day.split('s')[1].length)
+console.log(day.split('s')[1].trim())
+
+let date = '23'
+let nextDate = '27'
+console.log(Number(nextDate - date))
+let diff = parseInt(nextDate) - parseInt(date)
+diff.toString()
+
+month = '08'
+
+let temp = day +'is funday. Monday is a boring day'
+console.log(temp)
+console.log(temp.indexOf('day'))
+console.log(temp.indexOf('day',5))
+console.log(temp.lastIndexOf('day'))
+let index = 0
+// let count = 1
+// while(temp.indexOf('day',index)!=temp.lastIndexOf('day')){
+//     count++
+//     index=temp.indexOf('day',index)+1
+// }
+// console.log(count)
+
+let count = 0
+let val =temp.indexOf('day',index) 
+while(val != -1){
+    count++
+    val=temp.indexOf('day',val+1)
+}
+console.log(count)
